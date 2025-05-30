4fd03df6d86acc249fd45066a0801afd3faa755e
Serge Toure <26191503+sergetoure09@users.noreply.github.com>
2025-05-30 14:48:02 +0200

    layout done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.scss";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>My Calculation Application</h1>
+      <article className="calc">
+        <article className="calc-logo">
+          <img src={reactLogo} alt="logo" className="calc-logo__img" />
+          <span className="calc-logo__title">cientis instrument</span>
+        </article>
+        <div>
+          <input className="display-pane" type="text" name="" id="" />
+        </div>
+
+        <article className="control-pane">
+          <article className="control-pane__digit-pane">
+            <div className="digit">AC</div>
+            <div className="digit">1</div>
+            <div className="digit">2</div>
+            <div className="digit">3</div>
+            <div className="digit">4</div>
+            <div className="digit">5</div>
+            <div className="digit">6</div>
+            <div className="digit">7</div>
+            <div className="digit">8</div>
+            <div className="digit">9</div>
+            <div className="digit">0</div>
+            <div className="digit">.</div>
+          </article>
+          <article className="control-pane__operation-pane">
+            <div className="digit digit--operation">+</div>
+            <div className="digit digit--operation">-</div>
+            <div className="digit digit--operation">:</div>
+            <div className="digit digit--operation">x</div>
+          </article>
+        </article>
+      </article>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
